8922dbaadd94d05d679ba8db598485471e43f844
CleverDataScientist <99399322+CleverDataScientist@users.noreply.github.com>
2022-02-14 05:13:43 -0800

    Add files via upload

---
+++ check whether a person is eligible for  voting or not.py
@@ -0,0 +1,8 @@
+# write a program to check whether a person is eligible for voting or not. 
+def age(x):
+    if x>=18:
+        print("eligible for voting")
+    else:
+        print("not eligible voting")
+age(21)
+age(16)
