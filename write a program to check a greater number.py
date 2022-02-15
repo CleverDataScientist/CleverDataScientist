97124057df292644d8e0d4c707a6d198ab899df2
CleverDataScientist <99399322+CleverDataScientist@users.noreply.github.com>
2022-02-15 00:54:47 -0800

    Add files via upload

---
+++ write a program to check a greater number.py
@@ -0,0 +1,9 @@
+# write a program to check a greater number.
+def number(x,y):
+    if y>x:
+        print("y is greater than x")
+    else:
+        print("x is greater than y")
+number(20,50)
+number(90,50)
+        
