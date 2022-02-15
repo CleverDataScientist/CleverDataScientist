1a481ad5330f618bf9b4a1a35b2779a1b4a54f98
CleverDataScientist <99399322+CleverDataScientist@users.noreply.github.com>
2022-02-15 00:46:07 -0800

    Add files via upload

---
+++ write a program to check  acharacter is vowel or not.py
@@ -0,0 +1,10 @@
+# write as program to check a character is vowel or not.
+def character(x):
+    vowel = "aeiou"
+    if x in vowel :
+        print("entered character is vowel")
+    else:
+        print("entered character is not vowel")
+character("h")
+character("a")
+        
