c2f9fe45ee0b8211cd95495e1b5e6837e9d6dce6
CleverDataScientist <99399322+CleverDataScientist@users.noreply.github.com>
2022-02-15 01:23:27 -0800

    Add files via upload

---
+++ write a program to accept a number from 1to12 and display name of the month .py
@@ -0,0 +1,31 @@
+# write a program to accept a number from 1to12 and display name of the month 
+
+def month(n):
+    if n==1:
+        print("january")
+    elif n==2:
+        print("february")
+    elif n==3:
+        print("march")
+    elif n==4:
+        print("april")
+    elif n==5:
+        print("may")
+    elif n==6:
+        print("june")
+    elif n==7:
+        print("july")
+    elif n==8:
+        print("august")
+    elif n==9:
+        print("september") 
+    elif n==10:
+        print("october")  
+    elif n==11:
+        print("november")
+    elif n==12:
+        print("decemeber")
+    else:
+        print("plese enter number between 1 to 12")
+month(5)
+month(13)
